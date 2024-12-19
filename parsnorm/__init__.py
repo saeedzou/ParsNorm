39abd2fb181b1cb8a3b2fa7c161454e5851bfd08
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2024-12-19 03:49:34 +0330

    update init

---
+++ parsnorm/__init__.py
@@ -0,0 +1,2 @@
+from .parsnorm import ParsNorm
+from .en_fa_transliterate import EnFaTransliterate
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='parsnorm',  # The name of your package
+    packages=['parsnorm'],
     version='0.1.0',  # Initial version
     description='Text Normalization Tool for Persian Speech Dataset Creation',
     long_description=open('README.md').read(),  # Read the README for a detailed description
